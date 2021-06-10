5ef0ec29748ae538a86d9080715dda4e58c58981
Erik Heath <erikheath8@gmail.com>
2021-06-10 00:25:06 -0400

    upto pg 131 Extending the HeadlinesView

---
+++ News/News/News/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace News
+{
+    public static class Settings
+    {
+        public static string NewsApiKey
+        {
+            get
+            {
+                return "4e2e50e9d9ae4439bb7513594bc81c80";
+            }
+        }
+    }
+}
